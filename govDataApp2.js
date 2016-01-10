c33bc40684d2d46dc0f9e4731bc2f2dc7e54f328
Mike McGuire <MoonshotMike@gmail.com>
2016-01-09 17:02:06 -0700

    working

--- govDataApp2.js
+++ govDataApp2.js
@@ -5,13 +5,18 @@ myApp.controller('mainController', ['$scope', '$http', function ($scope, $http)
     $http.get('https://data.ny.gov/api/views/d6yy-54nr/rows.json?accessType=DOWNLOAD')
         .success(function (result) 
             {
-            $scope.lotteryRecords = result;
-            console.log("Hooray!");
+//            $scope.lotteryRecords = result;
+            console.log("Hooray!  " + result.data[0][9]);
+            $scope.winningNumbers = [];
+            for(var i = 0; i < 50; i++)
+                {
+                $scope.winningNumbers.push(result.data[i][9]);
+                }
             })
         .error(function (data, status) 
             {
             console.log("Oops...");
-            });  
+            });
     }]);
 
 
