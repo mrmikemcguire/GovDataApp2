da4a1f4a5cad889f311c8a0fe6845fcf433707e2
Mike McGuire <MoonshotMike@gmail.com>
2016-01-10 15:04:14 -0700

    fix hard-coded loop

--- govDataApp2.js
+++ govDataApp2.js
@@ -5,12 +5,11 @@ myApp.controller('mainController', ['$scope', '$http', function ($scope, $http)
     $http.get('https://data.ny.gov/api/views/d6yy-54nr/rows.json?accessType=DOWNLOAD')
         .success(function (result) 
             {
-//            $scope.lotteryRecords = result;
-            console.log("Hooray!  " + result.data[0][9]);
-            $scope.winningNumbers = [];
-            for(var i = 0; i < 50; i++)
+            console.log("Hooray!");
+            $scope.winningSeries = [];
+            for(var i = 0; i < result.data.length; i++)
                 {
-                $scope.winningNumbers.push(result.data[i][9]);
+                $scope.winningSeries.push(result.data[i][9]);
                 }
             })
         .error(function (data, status) 
